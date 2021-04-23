09ca305c3c7372dd5b63277a64b07f468d398dc6
bigdatamoore <9993122+bigdatamoore@users.noreply.github.com>
2021-04-23 12:36:35 -0400

    added comparison of dataframes

--- dataunit/commands/assert_datasets_equal.py
+++ dataunit/commands/assert_datasets_equal.py
@@ -1,5 +1,7 @@
 from dataunit.commands.base import DataUnitCommand
 from dataunit.context import Context
+from excel.dataframe_utils import get_dataframe_from_worksheet
+import pandas as pd
 
 class AssertDatasetsEqualCommand(DataUnitCommand):
     """
@@ -8,7 +10,7 @@ class AssertDatasetsEqualCommand(DataUnitCommand):
     """
     command_name = "Assert Datasets Equal"
 
-    def __init__(self, workbook_path, workbook, settings, expected_dataset, expected_dataset_type, actual_dataset, actual_dataset_type):
+    def __init__(self, workbook_path, workbook, settings, expected_dataset, expected_dataset_type, actual_dataset, actual_data_type):
         self.workbook_path = workbook_path
         self.workbook = workbook
         self.settings = settings
@@ -17,6 +19,7 @@ class AssertDatasetsEqualCommand(DataUnitCommand):
         self.actual_dataset = actual_dataset
         self.actual_dataset_type = actual_dataset_type
 
+    
 
     def run(self, context: Context) -> str:
         """
@@ -26,16 +29,20 @@ class AssertDatasetsEqualCommand(DataUnitCommand):
         :return:
         """
         
+        # need to get the expected dataset based on its type as a pandas df
         if self.expected_dataset_type.upper() == "WORKSHEET":
-
-            #df = get_dataframe_from_worksheet(self.workbook, self.expected_dataset)
-            pass
+            expected_df = get_dataframe_from_worksheet(self.workbook, self.expected_dataset)
         else if self.expected_dataset_type.upper() == "RESULTSET":
+            expected_df = Context[self.expected_dataset]
             
-            df = Context[self.expected_dataset]
-            
-        #TODO get the expected and actual datasets at pandas dataframes and then compare them using pandas equal
-        #TODO will need a helper method to load the pandas from worksheet
+        # get the actual dataset from the context as pandas df
+        if self.actual_dataset_type.upper() == "WORKSHEET":
+            actual_df = get_dataframe_from_worksheet(self.workbook, self.expected_dataset)
+        else if self.expected_dataset_type.upper() == "RESULTSET":
+            actual_df = Context[self.actual_dataset]
+
+
+        assert expected_df.equals(actual_df), f"Expected dataset {self.expected_dataset} does not match actual dataset {self.actual_dataset}."
         
 
     
